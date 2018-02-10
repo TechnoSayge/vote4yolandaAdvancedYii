cdf6f636936be394a1df207986eb5192cdf632a6
YSC <technosayge@gmail.com>
2018-02-10 17:11:05 -0500

    Adding Advance Yii Framework

---
+++ advanced/frontend/views/site/index.php
@@ -0,0 +1,65 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Vote Yolanda Smith Charles!</h1>
+
+        <p class="lead">Oakland County Commissioner
+        <break>
+        District 17
+        </p>
+
+       
+       <!-- <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div> -->
+
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Proven Leadership</h2>
+
+                <p>3 term member of the Southfield Board
+                of Education, <em>Yolanda Smith Charles</em> is a proven leader.  She has held the position of 
+                President, Vice President, Treasurer and Secretary.  Leadership may not always be politically 
+                popular but in every vote YSC has a steadfast approach and stands by every vote, 
+                every time.  </p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Desire to Serve</h2>
+
+                <p>Yolanda Smith Charles has a servants heart.  With your help she will be 
+                in the position to better serve the people of Lathrup Village, Oak Park and
+                Southfield.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="3KDMN3AL28526">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+</div>
